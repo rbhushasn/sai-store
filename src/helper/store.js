dc6c22221c70c462f1d509eb6065e8c647c1e046
rbhushasn <ravi.bhushan@sial.com>
2019-05-13 20:33:01 +0530

    store changes

--- src/helper/store.js
+++ src/helper/store.js
@@ -1,8 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-
-
-import reducers from "reducers/";
+import reducers from "reducers";
 
 let middleware = (compose)(applyMiddleware(thunk));
 
